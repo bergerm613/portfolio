330ee41e6e24da002e5de462639f7c647e98c992
MB <bergerm613@gmail.com>
2021-02-15 17:10:08 -0500

    experience content and animation

--- src/pages/ExperiencePage.js
+++ src/pages/ExperiencePage.js
@@ -1,30 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import indicative from '../images/companies/indicative.png'
+import rotation from '../images/companies/rotation.png'
+import gwc from '../images/companies/gwc.png'
 
 class ExperiencePage extends Component {
     render() {
         return (
             <div className="exp_page">
-                <div className="work_title">Work Experience</div>
+                <div className="work_title comfortaa">Work Experience</div>
 
                 <div className="indicative_box">
-                    Indicative Logo
-                    blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah 
+                    <BoxContent
+                        logo={indicative}
+                        description="a customer analytics platform"
+                        languages="REACT | ANGULAR | JAVASCRIPT | TYPESCRIPT"
+                        link="https://indicative.com">
+                        I was very fortunate to make a real impact while interning at Indicative
+                        with a mixture of feature implementations, optimizations, and bug fixes.
+                        This included a new time zone property, which is now used in all of Indicative’s tools.
+                    </BoxContent>
                 </div>
                 <div className="row_two">
                     <div className="rotation_box">
-                        Rotation Logo
-                        blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah 
+                        <BoxContent
+                            logo={rotation}
+                            description="world’s first streetwear rental company"
+                            languages="REACT | RUBY ON RAILS | FIGMA | HTML/CSS"
+                            link="https://therotation.club">
+                            At The Rotation, I developed user-friendly features in React and Ruby on Rails,
+                            including Netflix-style next package queue, catalog search,
+                            and shipping workflow tools for operations employees.
+                        </BoxContent>
                     </div>
                     <div className="gwc_box">
-                        GWC logo
-                        blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah blahblahblah 
+                        <BoxContent
+                            logo={gwc}
+                            description="nonprofit focused on the support and increase of women in computer science"
+                            languages="ARDUINO-C | LILYPAD ARDUINO | SOFT CURCUITS"
+                            link="https://girlswhocode.com/">
+                            As a teacher’s assistant at GWC Campus program, 
+                            I was able to help guide and support a class of middle school future-engineers 
+                            as they learned the basics of Arduino coding and wearable technology.
+                        </BoxContent>
                     </div>
                 </div>
 
-                <a href="https://github.com/bergerm613" target="_blank" className="github_button comfortaa">and check out my github<br/> for plenty of projects</a>
+                <a href="https://github.com/bergerm613" target="_blank" className="github_button comfortaa">and check out my github<br /> for plenty of projects!</a>
             </div>
         );
     }
+}
+
+const BoxContent = (props) => {
+    return (
+        <div className="company_wrapper">
+            <div>
+                <img src={props.logo} className="company_logo" />
+                <div className="company_desc">{props.description}</div>
+            </div>
+
+            <div className="opacity_zero company_par">{props.children}</div>
+
+            <div className="opacity_zero monospace">{props.languages}</div>
+            <a className="opacity_zero check_it_out comfortaa" target="_blank" href={props.link}>check it out</a>
+        </div>
+    )
 }
 
 export default ExperiencePage;
