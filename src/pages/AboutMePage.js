781f8f31c6827d50aae50a000f3c53afdb4ab1aa
MB <bergerm613@gmail.com>
2021-09-13 16:01:41 -0400

    update about content

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -4,9 +4,8 @@ import { Tooltip } from 'react-tippy'
 import Sparkle from 'react-sparkle'
 import hand from '../images/tooltips/hand.png'
 import touro from '../images/tooltips/touro.jpg'
-import rotation from '../images/tooltips/rotation.png'
-import indicative from '../images/tooltips/indicative.png'
 import cinnamon from '../images/tooltips/cinnoman.jpg'
+import { Link } from 'react-router-dom'
 
 class AboutMePage extends Component {
     render() {
@@ -14,27 +13,29 @@ class AboutMePage extends Component {
             <div className="about_page">
                 <div className="ocean_image"><Sparkle fadeOutSpeed={30}/></div>
                 <div className="rectangle">
-                    <div className="about_title monospace">All About Me</div>
+                    <div className="about_title monospace">A Little About Me</div>
                     <div className="about_text">
-                        Hello there, I see you have found my website. 
-                        <Tooltip theme="light" html={<img src={hand} alt="" height='35px' />}> Welcome!</Tooltip> I hope you enjoy your stay.
-
-                        I'm Michelle, and I just graduated with a Computer Science degree from
-                        <Tooltip theme="light" html={<img src={touro} alt="" height='35px' />}> Touro College</Tooltip>. 
-                        It was a great experience where I built a solid foundation in my knowledge of languages, data structures, and 
-                        <Tooltip theme="light" title="What's the shortest way to someone heart?<br/> The Dijkstra's algorithm."> algorithms</Tooltip>.
+                        Hello there, I see you have found my website.
+                        <Tooltip theme="light" html={<img src={hand} alt="" height='35px' />}> Welcome! </Tooltip>
+                        My name is Michelle, and I am a full stack developer with a love of beautiful code, clean design, and collaborative work. 
                         <br /><br />
 
-                        Besides for that, I had two wonderful internships, 
-                        where I worked with teams of engineers to build new features for web apps, one at a 
-                        <Tooltip theme="light" html={<img src={rotation} alt="" height='35px' />}> men's fashion subsciption startup</Tooltip>, 
-                        and the other at a company <Tooltip theme="light" html={<img src={indicative} alt="" height='35px' />}>building data analytic tools</Tooltip>. 
-                        Both gave me a love for collaborative work, and a deep appriciation for a job well done.
+
+                        I graduated with a degree in <Tooltip theme="light" title="Why are students cautious to approach CS? <br/> It bytes"> Computer Science</Tooltip> from  
+                        <Tooltip theme="light" html={<img src={touro} alt="" height='35px' />}> Touro College, </Tooltip>
+                        and since then had wonderful opportunities to put that theory into practice, 
+                        and contribute to an array of amazing teams and projects 
+                        (but more about that <Link to="/experience">here</Link>). 
+
+                        When not deep in code, I can often be found with 
+                        <Tooltip theme="light" title="mostly non-fiction, but open to all recommendations"> a new book </Tooltip>
+                        or making the
+                        <Tooltip theme="light" html={<img src={cinnamon} alt="" height='35px' />}> world's best cinnamon buns.</Tooltip>
                         <br /><br />
 
-                        When I'm not knee deep in code, I can be found curled up with 
-                        <Tooltip theme="light" title="Any will do, but memoirs are my favorite."> a book</Tooltip>, 
-                        or making the world's best <Tooltip theme="light" html={<img src={cinnamon} alt="" height='35px' />}>cinnamon buns</Tooltip> (no lie).
+                        
+                        I've designed and built this website to show you a little of what I can do with web development. So take some time to look around,
+                        and <Link to="/contact">don't hesitate to reach out</Link> - I'm always happy to chat.           
                     </div>
                 </div>
             </div>
