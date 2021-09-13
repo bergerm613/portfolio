e27416892251859dd604d00d6237a37ea14be8a5
MB <bergerm613@gmail.com>
2021-09-13 19:25:57 -0400

    adding quartet to experience page

--- src/pages/ExperiencePage.js
+++ src/pages/ExperiencePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import indicative from "../images/companies/indicative.png";
 import rotation from "../images/companies/rotation.png";
 import gwc from "../images/companies/gwc.png";
+import quartet from "../images/companies/quartet.png";
 
 class ExperiencePage extends Component {
   render() {
@@ -9,21 +10,36 @@ class ExperiencePage extends Component {
       <div className="exp_page">
         <div className="work_title monospace">Work Experience</div>
 
-        <div className="indicative_box">
+        <div className="box_full_width background_burnt_orange">
           <BoxContent
-            logo={indicative}
-            description="a customer analytics platform"
-            languages="REACT | ANGULAR | TYPESCRIPT"
-            link="https://indicative.com"
+            logo={quartet}
+            description="a platform that makes it easier for people to get the best mental health care for them"
+            languages="REACT | TYPESCRIPT | JAVA"
+            link="https://quartethealth.com"
           >
-            I was very fortunate to make a real impact while interning at
-            Indicative with a mixture of feature implementations, optimizations,
-            and bug fixes. This included a new time zone property, which is now
-            used in all of Indicative’s tools.
+            Working at Quartet, I collaborated with designers, project managers,
+            and engineers in an agile workflow to ensure new projects were
+            thoughtfully defined and successfully completed. In addition, I
+            implemented changes and optimizations to improve the existing
+            codebase.
           </BoxContent>
         </div>
-        <div className="row_two">
-          <div className="rotation_box">
+
+        <div>
+          <div className="box_half_width background_light_gray">
+            <BoxContent
+              logo={indicative}
+              description="a customer analytics platform"
+              languages="REACT | ANGULAR | TYPESCRIPT"
+              link="https://indicative.com"
+            >
+              I was very excited to make a real impact while interning at
+              Indicative with a mixture of feature implementations,
+              optimizations, and bug fixes. This included a new time zone
+              property, which is now used in all of Indicative’s tools.
+            </BoxContent>
+          </div>
+          <div className="box_half_width background_navy">
             <BoxContent
               logo={rotation}
               description="the world’s first streetwear rental company"
@@ -35,19 +51,19 @@ class ExperiencePage extends Component {
               search, and shipping workflow tools for operations employees.
             </BoxContent>
           </div>
-          <div className="gwc_box">
-            <BoxContent
-              logo={gwc}
-              description="nonprofit focused on the support and increase of women in computer science"
-              languages="ARDUINO-C | LILYPAD ARDUINO | SOFT CURCUITS"
-              link="https://girlswhocode.com/"
-            >
-              As a teacher’s assistant at the GWC Campus program, I was able to
-              help guide and support a class of middle school future-engineers
-              as they learned the basics of Arduino coding and wearable
-              technology.
-            </BoxContent>
-          </div>
+        </div>
+
+        <div className="box_full_width background_light_blue">
+          <BoxContent
+            logo={gwc}
+            description="nonprofit focused on the support and increase of women in computer science"
+            languages="ARDUINO-C | LILYPAD ARDUINO | SOFT CURCUITS"
+            link="https://girlswhocode.com/"
+          >
+            As a teacher’s assistant at the GWC Campus program, I was able to
+            help guide and support a class of middle school future-engineers as
+            they learned the basics of Arduino coding and wearable technology.
+          </BoxContent>
         </div>
 
         <a
